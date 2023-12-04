55513f4cd5ab7c5c97a95140767da830f78bedd6
Firuddin <firuddinaliyev888@gmail.com>
2023-12-05 00:36:01 +0400

    navbar login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { IoIosBasket } from "react-icons/io";
 
 const Header = () => {
   return (
     <>
     <header>
       <navbar>
-        <div className='container'>
+        <div className='container max-w-[1200px] mx-auto py-6 flex justify-between'>
         <div className='logo'>
           <img src="./Delizioso.png" alt="" />
         </div>
-        <div className='links'></div>
-        <div className='login'></div>
+        <div className='links'>
+          <ul className='flex gap-10'>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>About us</li>
+            <li>Order online</li>
+            <li>Reservation</li>
+            <li>Contaact us</li>
+          </ul>
+        </div>
+        <div className='login'>
+          <div><IoIosBasket /></div>
+          <div></div>
+        </div>
         </div>
       </navbar>
     </header>
