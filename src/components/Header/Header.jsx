2204edebdc635796494911c6723f61d4e8858beb
Firuddin <firuddinaliyev888@gmail.com>
2023-12-05 23:51:05 +0400

    navbar the end

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
+import React from "react";
 import { IoIosBasket } from "react-icons/io";
 
 const Header = () => {
   return (
     <>
-    <header>
-      <navbar>
-        <div className='container max-w-[1200px] mx-auto py-6 flex justify-between'>
-        <div className='logo'>
-          <img src="./Delizioso.png" alt="" />
-        </div>
-        <div className='links'>
-          <ul className='flex gap-10'>
-            <li>Home</li>
-            <li>Menu</li>
-            <li>About us</li>
-            <li>Order online</li>
-            <li>Reservation</li>
-            <li>Contaact us</li>
-          </ul>
-        </div>
-        <div className='login'>
-          <div><IoIosBasket /></div>
-          <div></div>
-        </div>
-        </div>
-      </navbar>
-    </header>
+      <header>
+        <navbar>
+          <div className="container max-w-[1200px] mx-auto py-6 flex justify-between items-center">
+            <div className="logo">
+              <img src="./Delizioso.png" alt="" />
+            </div>
+            <div className="links">
+              <ul className="flex gap-10">
+                <li>Home</li>
+                <li>Menu</li>
+                <li>About us</li>
+                <li>Order online</li>
+                <li>Reservation</li>
+                <li>Contaact us</li>
+              </ul>
+            </div>
+            <div className="flex gap-4 items-center">
+              <IoIosBasket />
+              <button className="bg-[green] py-[15px] px-[36px] rounded-[133px]">login</button>
+            </div>
+          </div>
+        </navbar>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
